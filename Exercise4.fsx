9d0d5b3786dc193929133c7d44731c1023ab618f
Anders Bech Mellson <mellson@icloud.com>
2014-11-28 13:00:10 +0100

    init

---
+++ Exercise4.fsx
@@ -0,0 +1,11 @@
+﻿#r "../packages/FSharp.Data.2.1.0/lib/net40/FSharp.Data.dll"
+open FSharp.Data
+
+type Measurement = CsvProvider<"sorted100M_sample.csv",CacheRows=false>
+let mutable measurements = Measurement.Load("xac").Rows
+let splitFiles = [1..1]
+for i in splitFiles do
+  measurements <- Seq.append measurements (Measurement.Load("xac").Rows)
+
+for m in measurements do
+  printfn "%A" m
---
+++ Program.fs
@@ -0,0 +1,35 @@
+﻿open FSharp.Data
+open FileHelpers
+
+[< DelimitedRecord(",") >]
+type Measurement() =
+    class
+      [<DefaultValue>]
+      val mutable ReadingId : int
+      [<DefaultValue>]
+      val mutable Timestamp : int
+      [<DefaultValue>]
+      val mutable Value : decimal
+      [<DefaultValue>]
+      val mutable Property : bool
+      [<DefaultValue>]
+      val mutable Plug_Id : int
+      [<DefaultValue>]
+      val mutable Household_Id : int
+      [<DefaultValue>]
+      val mutable House_Id : int
+    end
+
+let downcast_Measurement_Array = Array.map (fun (a:obj) -> a :?> Measurement)
+
+[<EntryPoint>]
+let main argv = 
+    let data = new CsvProvider<"sorted100M_sample.csv">()
+    printfn "%A" (data.Rows |> Seq.head)
+
+    let engine = new FileHelperEngine(typeof<Measurement>)
+    let res = engine.ReadFile("C:\Users\Anders\Desktop\Git\SBDM-Exercise-4\SBDM-Exercise-4\sorted100M_sample_noHeader.txt")
+    let measurements = downcast_Measurement_Array res
+
+    printfn "%A" measurements.Length
+    0
